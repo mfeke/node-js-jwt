9e7cc04bd5b4b397012911de480c2343bcf00f0f
mfeke <mfekethothulane@gmail.com>
2022-09-28 18:56:25 +0200

    update

--- server.js
+++ server.js
@@ -64,6 +64,9 @@ db.mongoose
     })
   }
 
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app)
+
 app.get("/" ,(req ,res) =>{
     res.json({message:"Welcome to thulane application"})
 })
